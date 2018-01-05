d246f099214022ea63206232069069bbf806aa98
newegg <newegg@MBP-Barry.buyabs.corp>
2018-01-05 16:37:00 +0800

    update port

--- server.js
+++ server.js
@@ -5,15 +5,15 @@ var http = require('http');
 var fs = require('fs');
 // var http = chatServer.createServer(app);
 var options = {
-	key: fs.readFileSync('./certification/214399648100586.key'),
-	cert: fs.readFileSync('./certification/214399648100586.pem')
+    key: fs.readFileSync('./certification/214399648100586.key'),
+    cert: fs.readFileSync('./certification/214399648100586.pem')
 }
 
 
 // io.emit('some event', {for: 'everyone' });
 // http.createServer(app).listen(3500)
-http.createServer(app).listen(80)
-https.createServer(options, app).listen(443)
-// http.listen(80,function(){
-// 	console.log('listening on *:80');
-// });
+http.createServer(app).listen(3500)
+    // https.createServer(options, app).listen(443)
+    // http.listen(80,function(){
+    // 	console.log('listening on *:80');
+    // });
