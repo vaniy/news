3977329deffad92b9a1fbbbce093d8ca4484b644
newegg <newegg@MBP-Barry.buyabs.corp>
2018-01-18 11:10:42 +0800

    statistics

--- routes/index.js
+++ routes/index.js
@@ -193,7 +193,7 @@ router.get("/api/all", function(req, res, next) {
 router.get("/api/categoryDetails", function(req, res, next) {
     const sql = require('mssql')
     new sql.ConnectionPool(config).connect().then(pool => {
-        return pool.request().query(`select * from productsort a inner join glzhidu b on a.id = b.leixing ${req.query.order ? 'DESC b.dianji':'order by shijian'}`)
+        return pool.request().query(`select * from productsort a inner join glzhidu b on a.id = b.leixing order by ${req.query.order ? 'b.dianji DESC':'shijian'}`)
     }).then(result => {
         let rows = result.recordset
         res.setHeader('Access-Control-Allow-Origin', '*')
